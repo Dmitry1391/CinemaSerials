5a3224cc7324cfe7ac5c8eff75a841217bc9f66f
Dmitry Guborevich <pavelguborevich@yandex.ru>
2020-07-31 13:39:21 +0300

    Add Splash animation and other res

--- app/src/main/java/com/example/cinemaserials/SplashScreenActivity.kt
+++ app/src/main/java/com/example/cinemaserials/SplashScreenActivity.kt
@@ -1,11 +1,41 @@
 package com.example.cinemaserials
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val animationRotateCenter = AnimationUtils.loadAnimation(this, R.anim.rotate_center)
+        splash_app_name.startAnimation(animationRotateCenter)
+
+        /* New Handler to start the Main-Activity and close this SplashScreen after some seconds.*/
+        Handler().postDelayed({ startMainActivity() }, 3000)
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        startActivity(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
 }
